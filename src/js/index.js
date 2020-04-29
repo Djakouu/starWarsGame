e9d1c8a660131c60b5680437e4cee5bb6f156890
Djakou <kimouche-mz@hotmail.fr>
2020-04-29 13:40:19 +0200

    Final project

--- src/js/index.js
+++ src/js/index.js
@@ -29,6 +29,7 @@
 
 
 
+
 import Position from "./models/Position"
 import Robot from "./models/Robot"
 import * as spriteView from "./views/spriteView"
@@ -38,6 +39,17 @@ import * as startGame from "./controlers/startGame"
 import * as transition from "./views/transition"
 import { elements } from "./views/base"
 import * as musicControler from "./controlers/music"
+
+const isMobileDevice = () => {
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+if (!isMobileDevice()) 
+  document.getElementById("esc").innerHTML = " Press 'ESC' key to pause ";
+else {
+  document.getElementById("esc").innerHTML = " Tap the 'screen' to pause ";
+}
+
+let orientation = "";
 
 export const game = new class {
   constructor(
@@ -72,41 +84,53 @@ export const game = new class {
     this.music=music;
   }
   async start() {
+    if (isMobileDevice())
+      orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
     // Hide the control panel and widen the playgound if window.innerWidth < 900
     let promise = new Promise((res, rej) => {
-      if (window.innerWidth < 900) {
+      if (window.innerWidth < 900 && !orientation.includes("landscape")) {
         // transition.hideElements(["controlPanel"], "fadeOutRight")
         elements("controlPanel").style.width = "0vw";
-        elements("playground").style.width = "100vw";
+        elements("playgroundAndDashboard").style.width = "100vw";
         elements("esc").style.visibility = "visible";
         setTimeout(() => {
           elements("controlPanel").style.display = "none";
+          elements("playground").style.height = "90%";
+          elements("touchDevicesDashboard").style.height = "10%";
+          setTimeout(() => {
+            elements("touchDevicesDashboard").style.display = "flex";
+          }, 200);
         }, 200);
         setTimeout(() => {
-          res(true);
+          res(1);
         }, 1000);
       }
-      else res(false);
+      else res(0);
     });
-    await promise
+    await promise;
     // Demarrage du jeux  
     this.run = true;    
-    startGame.startLevel(this.level)
+    startGame.startLevel(this.level);
     // Lunch time
     controlPanelView.lunchTime(); 
     // Toggle buttons
-    controlPanelView.toggleButtons("start")
+    controlPanelView.toggleButtons("start");
   }
 
   stop() {
     // Show the controlPanel if hidden (afte a window resize)
     if (elements("controlPanel").style.width == "0vw") {
-      elements("controlPanel").style.width = "20vw";
-      elements("playground").style.width = "80vw";
-      elements("esc").style.visibility = "hidden";
+      elements("playground").style.height = "100%";
+        elements("touchDevicesDashboard").style.height = "0%";
       setTimeout(() => {
-        elements("controlPanel").style.display = "flex";
-      }, 400);
+        elements("touchDevicesDashboard").style.display = "none";
+        elements("controlPanel").style.width = "20vw";
+        elements("playgroundAndDashboard").style.width = "80vw";
+        elements("esc").style.visibility = "hidden";
+        setTimeout(() => {
+          elements("controlPanel").style.display = "flex";
+        }, 400);
+      }, 200);
     }
     game.run=false;
     // Remove the robot
@@ -117,7 +141,7 @@ export const game = new class {
     }
     // Remove enemies
     this.enemies.forEach(enemy => {
-      spriteView.removeSprite(enemy.id)
+      spriteView.removeSprite(enemy.id);
     })
     this.enemies=[];
     // reset Arrows
@@ -134,20 +158,23 @@ export const game = new class {
     // Reset the controlPanel time
     controlPanelView.stopTime();
     // Toggle buttons
-    controlPanelView.toggleButtons("stop")
+    controlPanelView.toggleButtons("stop");
   }
 
   startOver() {
     // Switch music
     if (this.music != "RGwaGzIp7T8") {
-      musicControler.switchMusic("RGwaGzIp7T8")
-      this.music = "RGwaGzIp7T8"
+      musicControler.switchMusic("RGwaGzIp7T8");
+      this.music = "RGwaGzIp7T8";
     }
     transition.hideElements(["endGame"], "fadeOut");
-    if (elements("timeLeftInput").value == "TIME'S UP!")
-      elements("timeLeftInput").value = "02:00"
+    if (elements("timeLeftInput").value == "UP!") {
+      elements("timeLeftInput").value = "02:00";
+      elements("dashboardTimeLeftInput").value = "02:00";
+
+    }
     this.run=true;
-    main(0, true)
+    main(0, true);
     this.start(); 
   }
 
@@ -158,12 +185,17 @@ export const game = new class {
     // Show the control panel if hidden and tighten the playgound
     if (elements("controlPanel").style.width == "0vw") {
       // transition.hideElements(["controlPanel"], "fadeOutRight")
-      elements("controlPanel").style.width = "20vw";
-      elements("playground").style.width = "80vw";
-      elements("esc").style.visibility = "hidden";
+      elements("playground").style.height = "100%";
+      elements("touchDevicesDashboard").style.height = "0%";
       setTimeout(() => {
-        elements("controlPanel").style.display = "flex";
-      }, 400);
+        elements("touchDevicesDashboard").style.display = "none";
+        elements("controlPanel").style.width = "20vw";
+        elements("playgroundAndDashboard").style.width = "80vw";
+        elements("esc").style.visibility = "hidden";
+        setTimeout(() => {
+          elements("controlPanel").style.display = "flex";
+        }, 400);
+      }, 200);
     }
    
   }
@@ -171,13 +203,18 @@ export const game = new class {
   async resume() {
     // Hide the control panel if window.innerWidth < 900
     let promise = new Promise((res, rej) => {
-      if (window.innerWidth < 900) {
+      if (window.innerWidth < 900 && !orientation.includes("landscape")) {
         // transition.hideElements(["controlPanel"], "fadeOutRight")
         elements("controlPanel").style.width = "0vw";
-        elements("playground").style.width = "100vw";
+        elements("playgroundAndDashboard").style.width = "100vw";
         elements("esc").style.visibility = "visible";
         setTimeout(() => {
           elements("controlPanel").style.display = "none";
+          elements("playground").style.height = "90%";
+          elements("touchDevicesDashboard").style.height = "10%";
+          setTimeout(() => {
+            elements("touchDevicesDashboard").style.display = "flex";
+          }, 200);
         }, 200);
         setTimeout(() => {
           res(true);
@@ -195,9 +232,9 @@ export const game = new class {
 
   update(tFrame) {
     // Stop the game if time out
-    if (elements("timeLeftInput").value == "TIME'S UP!") {
+    if (elements("timeLeftInput").value == "UP!") {
       musicControler.switchMusic('NZruHFBBi6Q');
-      this.music = "NZruHFBBi6Q"
+      this.music = "NZruHFBBi6Q";
       this.stop();
       elements("you").innerHTML = "GAME OVER!";
       elements("your").innerHTML = "Time's up";
@@ -209,8 +246,8 @@ export const game = new class {
     if (this.run && this.robot && this.enemies.length <= ((this.level-1)*2)+1) {
     //((this.level-1)*2)+1): means 1 darthvader left for level 1,
     // Three darthvader for level 2 and 5 darthvader for level 3
-      let lastTime = elements("timeLeftInput").value
-      const lastScore = elements("scoreInput").value
+      let lastTime = elements("timeLeftInput").value;
+      const lastScore = elements("scoreInput").value;
       this.stop();
       setTimeout(() => {
         
@@ -249,8 +286,8 @@ export const game = new class {
           setTimeout(() => {
             // If the player didn't hit startOver or next/previous level less than 2sec ago
             if (!this.run && lastLevel == this.level) {
-              transition.hideElements(["endGame"], "fadeOut")
-              elements("you").innerHTML = "YOU WON!"
+              transition.hideElements(["endGame"], "fadeOut");
+              elements("you").innerHTML = "YOU WON!";
               elements("your").innerHTML = "Final score:"
               elements("finalScore").innerHTML = finalScore;
               transition.showElements(["endGame"], "fadeIn");
@@ -259,16 +296,16 @@ export const game = new class {
         }
         else { // finalScore <= 0
           musicControler.switchMusic('bI8u4k6wxek');
-          this.music = "bI8u4k6wxek"
+          this.music = "bI8u4k6wxek";
           transition.showElements(["endGame"], "fadeIn");
           const lastLevel = this.level;
           setTimeout(() => {
             // If the player didn't hit startOver or next/previous level less than 2sec ago
             if (!this.run && lastLevel == this.level) {
-              transition.hideElements(["endGame"], "fadeOut")
-              elements("you").innerHTML = "You lost!"
-              elements("your").innerHTML = "Final score:"
-              elements("finalScore").innerHTML = finalScore
+              transition.hideElements(["endGame"], "fadeOut");
+              elements("you").innerHTML = "You lost!";
+              elements("your").innerHTML = "Final score:";
+              elements("finalScore").innerHTML = finalScore;
               transition.showElements(["endGame"], "fadeIn");
             }
           }, 2000);          
@@ -276,47 +313,93 @@ export const game = new class {
       }, 200);
     };
 
-    // Pause the game if "ESC" key is pressed (if window.innerWidth is < 900)
-    document.onkeydown = k => {
-      if (window.innerWidth < 900 && k.key === "Escape") {
-        game.pause();
-        // Toggle buttons
-        controlPanelView.toggleButtons("pause");
-      }
-    };
+    
+    
 
     // Update the game according to the time
-    let lap = tFrame - this.tFrameLast ;
+    let lap = tFrame - this.tFrameLast;
     
-    // Update this.arrows on keydown
-    window.onkeydown = k => {
-      if (this.arrows[k.key] != true && 
-          (k.key === "ArrowLeft" ||
-           k.key === "ArrowUp" ||
-           k.key === "ArrowRight" || 
-           k.key === "ArrowDown")
-        ) {
-        this.arrows = positionView.updateArrowsValues(k.key);
+    if (this.run && this.robot) {
+      if (!isMobileDevice()) {
+        // Update this.arrows on keydown
+        window.onkeydown = k => {
+          if (this.arrows[k.key] != true && 
+              (k.key === "ArrowLeft" ||
+              k.key === "ArrowUp" ||
+              k.key === "ArrowRight" || 
+              k.key === "ArrowDown")
+            ) {
+            this.arrows = positionView.updateArrowsValues(k.key);
+          }
+          // Pause the game if "ESC" key is pressed (if window.innerWidth is < 900)
+          else if (window.innerWidth < 900 && k.key === "Escape") {
+            // or ontouchstart for touch diveces
+            game.pause();
+            // Toggle buttons
+            controlPanelView.toggleButtons("pause");
+          }
+        };
       }
-    };
+      else { // isMobileDevice
+        // pause on screen touch
+        if (!orientation.includes("landscape")) {
+          window.ontouchstart = () => {
+            game.pause();
+            // Toggle buttons
+            controlPanelView.toggleButtons("pause");
+          };
+        }
+        // Update this.arrows on keydown according to the mobile orientation
+        if (orientation.includes("landscape")) {
+          window.ondeviceorientation = orientation => {
+            let x = orientation.gamma; // In degree in the range [0,360]
+            let y = orientation.beta;  // In degree in the range [-180,180]
+            if (y < -15)
+              this.arrows = positionView.updateArrowsValues("ArrowLeft");
+            else if (y > 15)
+              this.arrows = positionView.updateArrowsValues("ArrowRight");
+            else if (x > 15) // -30 < y < 30
+              this.arrows = positionView.updateArrowsValues("ArrowUp");
+            else if (x < -15) // -30 < y < 30
+              this.arrows = positionView.updateArrowsValues("ArrowDown");
+          }
+        }
+        else { // orientation.includes("portrait")
+          window.ondeviceorientation = orientation => {
+            let x = orientation.gamma; // In degree in the range [0,360]
+            let y = orientation.beta;  // In degree in the range [-180,180]
+            if (y < -15)
+              this.arrows = positionView.updateArrowsValues("ArrowUp");
+            else if (y > 15)
+              this.arrows = positionView.updateArrowsValues("ArrowDown");
+            else if (x > 15) // -30 < y < 30
+              this.arrows = positionView.updateArrowsValues("ArrowRight");
+            else if (x < -15) // -30 < y < 30
+              this.arrows = positionView.updateArrowsValues("ArrowLeft");
+          }
+        }
+      }
+    }
 
     // Update the robot position
     if (this.robot) {
-        const {stepX, stepY} = positionView.updateRobotPosition(this.arrows, this.robot)
+        const {stepX, stepY} = positionView.updateRobotPosition(this.arrows, this.robot);
         this.robot.moveRel(new Position(stepX, stepY));
     }
+    
+    
 
     // Update enemies positions then check possible collision 
     this.enemies.forEach(enemy => {
     // 1. update enemies position
-      const {stepX, stepY} = positionView.updateEnemiesPosition(enemy, lap)
+      const {stepX, stepY} = positionView.updateEnemiesPosition(enemy, lap);
       enemy.moveRel(new Position(stepX, stepY));
     // 2. Check possible collision
-      const collisionEnemy = positionView.checkCollision(enemy, this.robot)
+      const collisionEnemy = positionView.checkCollision(enemy, this.robot);
       if (collisionEnemy) {
         if (!collisionEnemy.id.includes("darthvader")) {
           // Remove sprite from the view
-            spriteView.removeSprite(collisionEnemy.id)
+            spriteView.removeSprite(collisionEnemy.id);
           // Remove enemy from the array
             this.enemies.splice(this.enemies.indexOf(collisionEnemy), 1);
           // Update score
@@ -361,7 +444,8 @@ elements("nextLevel").onclick = () => {
   if (game.robot)
     game.stop();
   game.level += 1;
-  elements("levelInput").value = game.level + "/3"
+  elements("levelInput").value = game.level + "/3";
+  elements("dashboardLevelInput").value = game.level + "/3";
   if (game.level == 3)
     transition.hideButton("nextLevel");
   if (game.level > 1)
@@ -372,7 +456,8 @@ elements("previousLevel").onclick = () => {
   if (game.robot)
     game.stop();
   game.level -= 1;
-  elements("levelInput").value = game.level + "/3"
+  elements("levelInput").value = game.level + "/3";
+  elements("dashboardLevelInput").value = game.level + "/3";
   if (game.level == 1)
     transition.hideButton("previousLevel");
   if (game.level < 3)
@@ -383,7 +468,7 @@ elements("previousLevel").onclick = () => {
 
 // Starting and stopping the game by clicking on the button "Start" or "Stop"
 elements("start").onclick = () => {
-  const innerHTML = elements("start").innerHTML
+  const innerHTML = elements("start").innerHTML;
   if (innerHTML == "Start") {
     // Start the game
     game.start(); 
@@ -455,7 +540,7 @@ let lastLap; // Stores the last lp between tFrame and game.tFrameLast to be redu
           lock = true;
       }
       // Stores the last lap
-      lastLap = tFrame - game.tFrameLast
+      lastLap = tFrame - game.tFrameLast;
       // Runs game.update
       game.update(tFrame);
     // Cancels the animation if the "stop" button in clicked
@@ -465,19 +550,8 @@ let lastLap; // Stores the last lp between tFrame and game.tFrameLast to be redu
     // Updates game.tFrameLast after every game.update
     game.tFrameLast = tFrame;
     }
-  main(0); // Lunch cycle
+  main(0, false); // Lunch cycle
 })();
-
-// Stop game if 's' pressed
-window.onkeyup = (k) => {
-  if (k.key == "s")
-      game.stop();
-  if (k.key == "x") {
-      game.stop();
-      game.run=false;
-  }
-}
-
 
 ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
@@ -486,12 +560,12 @@ window.onkeyup = (k) => {
 elements("nextRule").onclick = () => {
   const ruleNb = parseInt(elements("ruleNb").innerHTML);
   // Show "previousRule" button
-  transition.showButton("previousRule")
+  transition.showButton("previousRule");
   // Update rules number and its animation
   transition.updateAndAnimate(ruleNb+1, "Down");
   // Toggle rule page
-  transition.hideElements(["ruleAd"+ruleNb], "zoomOut")
-  transition.showElements(["ruleAd"+(ruleNb+1)], "slideInLeft")
+  transition.hideElements(["ruleAd"+ruleNb], "zoomOut");
+  transition.showElements(["ruleAd"+(ruleNb+1)], "slideInLeft");
   // skip rule if last page
   if (ruleNb+1 == 4) {
     transition.skipRules();
@@ -507,16 +581,16 @@ elements("previousRule").onclick = () => {
     transition.hideButton("previousRule");
   }
   // Toggle rule page
-  transition.hideElements(["ruleAd"+ruleNb], "slideOutLeft")
-  transition.showElements(["ruleAd"+(ruleNb-1)], "zoomIn")
+  transition.hideElements(["ruleAd"+ruleNb], "slideOutLeft");
+  transition.showElements(["ruleAd"+(ruleNb-1)], "zoomIn");
   // Show "nextRule" button, "skipRules" button, "rulesAd title" and "rulesNb" if last page
   if (ruleNb-1 == 3) {
     // Show "nextRule" button
     transition.showButton("nextRule");
     // Show "skipRules" button
-    transition.showButton("skipRules")
+    transition.showButton("skipRules");
     // Show "rulesAdTilte" and "rulesNb"
-    transition.showElements(["rulesAdTitle", "rulesNb"], "zoomIn")
+    transition.showElements(["rulesAdTitle", "rulesNb"], "zoomIn");
   }
 }
 
@@ -525,7 +599,8 @@ elements("skipRules").onclick = () => transition.skipRules();
 elements("ready").onclick = () => {
   // Set game.level according to the form
   game.level = parseInt(getRadioCheckedValue("level"));
-  elements("levelInput").value = game.level + "/3"
+  elements("levelInput").value = game.level + "/3";
+  elements("dashboardLevelInput").value = game.level + "/3";
   //Hide "previousLevel" or "nextLevel" buttons if game.level = 1 or 3 respectively
   if (game.level == 1)
     transition.hideButton("previousLevel");
@@ -544,7 +619,7 @@ elements("ready").onclick = () => {
 }
 
 window.onload = () => {
-  musicControler.onYouTubePlayerAPIReady("q7jv3ecjgNc")
+  musicControler.onYouTubePlayerAPIReady("q7jv3ecjgNc");
   game.music = "q7jv3ecjgNc";
 }
 
